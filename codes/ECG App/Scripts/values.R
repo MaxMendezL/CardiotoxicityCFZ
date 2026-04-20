813378eb55a7c6c37cb6825aed52a1438b477f1f
Max Mendez <41516827+MaxMendezL@users.noreply.github.com>
2026-04-20 16:58:04 +0200

    Update values.R
    Minor changes:
    - included a call to a new analysis_ecg.R to dynamically display calculated values. 
    - Standardised DT rendering options. 
    
    Signed-off-by: Max Mendez <41516827+MaxMendezL@users.noreply.github.com>

--- codes/ECG App/Scripts/values.R
+++ codes/ECG App/Scripts/values.R
@@ -2,27 +2,35 @@ HeartRateUI <- function(id) {
   ns <- NS(id)
   
   tagList(
-    selectInput(ns("Input"), "Calculate Heart Rate", choices = c("Control", "BTZ", "CFZ", "ATRA", "CFZATRA")),
-    dataTableOutput(ns("values"))
+    selectInput(
+      ns("Input"),
+      "Calculate Heart Rate",
+      choices = c("Control", "BTZ", "CFZ", "ATRA", "CFZATRA")
+    ),
+    DT::dataTableOutput(ns("values"))
   )
 }
 
+
 HeartRate <- function(input, output, session) {
   
-output$values <- renderDataTable(
-  { 
-  if (input$Input  == "Control")       {datatable(ECGvals_CTRL, editable = FALSE)   
-  }else if (input$Input  == "BTZ")     {datatable(ECGvals_BTZ, editable = FALSE)
-  }else if (input$Input  == "CFZ")     {datatable(ECGvals_CFZ, editable = FALSE)
-  }else if (input$Input  == "ATRA")    {datatable(ECGvals_ATRA, editable = FALSE)
-  }else{ 
-    datatable(ECGvals_CFZATRA, editable = FALSE)
-   
-  }
-  }
-
-)
-
+  output$values <- DT::renderDataTable({
+    req(input$Input)
+    
+    res <- analyze_ecg_signal(input$Input)
+    dat <- res$table
+    
+    DT::datatable(
+      dat,
+      editable = FALSE,
+      rownames = FALSE,
+      options = list(
+        pageLength = 10,
+        scrollX = TRUE,
+        autoWidth = TRUE,
+        dom = "tip"
+      )
+    )
+  })
 }
-
 
